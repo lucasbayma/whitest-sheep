8501834c7284509021da4c90db5b8904e168e3a0
Rodolfo Neves Von Dollinger <rodolfo.neves@vallourec.com>
2019-07-20 20:19:41 -0300

    First commit

---
+++ feat_selection.py
@@ -0,0 +1,60 @@
+# Criacao da classe para features selection
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+class feat_selection:
+    
+    def __init__(self):
+        pass
+    
+    def forward_step(self):
+        pass
+    
+    def backward_step(self):
+        pass
+
+# Features selection based on genetic algorithm 
+class ga_selection:
+    
+    def __init__(self):
+        pass
+    
+    def pop_generation(self):
+        pass
+    
+    def cal_regression(self):  
+        # Function to calculate fiting model for regression 
+        pass
+    
+    def cal_classifier(self):
+        # Function to calculate fiting model for classification 
+        pass
+    
+    def select_parents(self):
+        # Select more adaptable elements
+        pass
+    
+    def crossover(self):
+        pass
+       
+    def mutation(self):
+        pass
+
+    def main(self):
+        pass
+    
+# Features selection using decision trees and forward stepwise
+class trees_forward:
+    
+    def __init__(self):
+        pass
+    
+    def features_importance_tree(self):
+        # Geting features importance using decision trees
+        pass
+    
+    def forward_trees(self):
+        # Apply forward stepwise for features importances 
+        pass
+    
